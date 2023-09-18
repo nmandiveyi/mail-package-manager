5626e6dcb42c0e7b0662eb1e4c8b513b3401d88c
Ngonidzashe Mandiveyi <nmandiveyi@Ngonidzashes-MacBook-Pro.local>
2023-09-17 22:15:37 -0400

    sign user sessions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { AuthController } from './auth/auth.controller';
+import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AuthController]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AuthModule,
+    PrismaModule,
+  ],
+  
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    JwtModule.register({}),
+    ],
   providers: [AuthService],
-  exports: [AuthService]
+  exports: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
